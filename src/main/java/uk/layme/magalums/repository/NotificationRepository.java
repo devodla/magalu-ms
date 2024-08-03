4419b29a54698dc2d2f98926f1f45057b2d3a9b4
Huillca, Reynaldo <lymreynaldo@icloud.com>
2024-08-03 19:08:26 -0300

    feat: add scheduler for send notifications.

--- src/main/java/uk/layme/magalums/repository/NotificationRepository.java
+++ src/main/java/uk/layme/magalums/repository/NotificationRepository.java
@@ -2,6 +2,11 @@ package uk.layme.magalums.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.layme.magalums.entity.Notification;
+import uk.layme.magalums.entity.Status;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByStatusInAndDateTimeBefore(List<Status> status, LocalDateTime dateTime);
 }
--- src/main/java/uk/layme/magalums/service/NotificationService.java
+++ src/main/java/uk/layme/magalums/service/NotificationService.java
@@ -1,15 +1,22 @@
 package uk.layme.magalums.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import uk.layme.magalums.controller.dto.ScheduleNotificationDto;
 import uk.layme.magalums.entity.Notification;
 import uk.layme.magalums.entity.Status;
 import uk.layme.magalums.repository.NotificationRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @Service
 public class NotificationService {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
 
     private final NotificationRepository notificationRepository;
 
@@ -32,5 +39,29 @@ public class NotificationService {
             notification.get().setStatus(Status.Values.CANCELED.toStatus());
             notificationRepository.save(notification.get());
         }
+    }
+
+    public void checkAndSend(LocalDateTime dateTime) {
+        List<Notification> notifications = notificationRepository.findByStatusInAndDateTimeBefore(
+                List.of(
+                        Status.Values.PENDING.toStatus(),
+                        Status.Values.ERROR.toStatus()
+                ),
+                dateTime
+        );
+
+        notifications.forEach(sendNotification());
+    }
+
+    private Consumer<Notification> sendNotification() {
+        return notification -> {
+            logger.info(
+                    "Send notification at {} from {}",
+                    notification.getDateTime(),
+                    notification.getChannel().getDescription());
+
+            notification.setStatus(Status.Values.SUCCESS.toStatus());
+            notificationRepository.save(notification);
+        };
     }
 }
