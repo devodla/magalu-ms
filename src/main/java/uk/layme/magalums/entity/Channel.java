dcc181cb19485e8192cd2f39695e1ea71d5520f0
Huillca, Reynaldo <lymreynaldo@icloud.com>
2024-08-03 16:30:37 -0300

    feat: add entities

---
+++ src/main/java/uk/layme/magalums/entity/Channel.java
@@ -0,0 +1,34 @@
+package uk.layme.magalums.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_channels")
+public class Channel {
+
+    @Id
+    private Long channelId;
+
+    private String description;
+
+    public Channel() {
+    }
+
+    public Long getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(Long channelId) {
+        this.channelId = channelId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
