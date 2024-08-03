1ffb773870641d3566450ca5a985a8d8922937b0
Huillca, Reynaldo <lymreynaldo@icloud.com>
2024-08-03 12:09:18 -0300

    chore: initial project

---
+++ src/main/java/uk/layme/magalums/MagaluMsApplication.java
@@ -0,0 +1,13 @@
+package uk.layme.magalums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagaluMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/layme/magalums/MagaluMsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.layme.magalums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
